4480e081b7bf67edabd50fffbd045d3a1dab07b7
basashi0425 <tai.hiratsuka@gmail.com>
2025-04-05 08:27:44 +0900

    Delete build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# 必要なパッケージをインストール
-apt-get update && apt-get install -y libnss3 libatk1.0-0 libatk-bridge2.0-0 libcups2 \
-    libxkbcommon-x11-0 libxcomposite1 libxrandr2 libgbm1 libpango-1.0-0 libasound2 \
-    libgtk-3-0 libx11-xcb1 fonts-liberation libwayland-client0 libwayland-cursor0 libwayland-egl1
-
-# Playwright の Firefox をインストール
-npx playwright install --with-deps firefox
